4f0cde3ebedc2db1b5a3bfba07106e4117b00d9a
wafaajbawi <wafaacj@hotmail.com>
2019-09-18 22:56:24 +0200

    the Board is added

--- Main.java
+++ Main.java
@@ -4,25 +4,29 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
-import javafx.scene.control.*;
 
-import java.awt.*;
 import java.io.FileInputStream;
+
 import javafx.geometry.*;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.effect.DropShadow;
 
 public class Main extends Application {
+    double radius = 30; // the inner radius from hexagon center to outer corner
+    double n = Math.sqrt(radius * radius * 0.75); // the inner radius from hexagon center to middle of the axis
+    double Hexagon_Height = 2 * radius;
+    double Hexagon_Width = 2 * n;
 
     DropShadow shadow = new DropShadow();
 
@@ -124,9 +128,19 @@ public class Main extends Application {
                 iv_2.setEffect(null);
             }
         });
-
-        VBox  vBox  = new VBox();
-        Scene playerScene = new Scene(vBox);
+        AnchorPane board = new AnchorPane();
+        int rowCount = 11; 
+        int HexPerRow = 10; 
+ 
+        for (int x = 0; x < HexPerRow; x++) {
+            for (int y = 0; y < rowCount; y++) {
+                double xCoord = x * Hexagon_Width + (y % 2) * n  +450;
+                double yCoord = y * Hexagon_Height * 0.75  + 140;
+                Polygon Hex = new Hexagon(xCoord, yCoord);
+                board.getChildren().add(Hex);               
+            }
+        }   
+        Scene playerScene = new Scene(board);
 
         VBox  box2  = new VBox();
         Scene AIScene = new Scene(box2);
@@ -387,6 +401,28 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
+    }
+    private class Hexagon extends Polygon {
+    	Hexagon(double x, double y) {
+            /* 
+            creates the polygon, defined by an array of (x,y) coordinates 
+    		which represents the six points of a hexagon
+    		*/
+            getPoints().addAll(
+                    x, y,
+                    x, y + radius,
+                    x + n, y + radius * 1.5,
+                    x + Hexagon_Width, y + radius,
+                    x + Hexagon_Width, y,
+                    x + n, y - radius * 0.5
+            );
+
+            // set up the visuals and a click listener for the tile
+            setFill(Color.ALICEBLUE);
+            setStrokeWidth(3);
+            setStroke(Color.BLACK);
+            setOnMouseClicked(e -> System.out.println("Clicked: " + this));
+        }
     }
 
 }
