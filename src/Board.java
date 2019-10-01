0135bd198a776d5f2ce6dc507cc551e79942522a
nibaj <nick.bast98@gmail.com>
2019-10-01 14:24:31 +0200

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,7 @@ import javafx.scene.shape.Polygon;
 
 public class Board extends Pane{
 	//create rows manually
-   private int HexRowStart = 6;
+   private int HexRowStart = 5;
    public double CenterX;
    public double CenterY;
    public Hashtable<String, Polygon> hashBoard = new Hashtable();
@@ -39,7 +39,7 @@ public Pane add() {
     		}
     		String neededForHash = Integer.toString(numberCode);
 */
-    		double xCoord = i * Hexagon.Hexagon_Width + (j % 2) * Hexagon.n  + CenterX-Hexagon.Hexagon_Width*4.5;
+    		double xCoord = i * Hexagon.Hexagon_Width + (j % 2) * Hexagon.n  + CenterX-Hexagon.Hexagon_Width*(HexRowStart-0.5);
     		double yCoord = j * Hexagon.Hexagon_Height * 0.75  + CenterY-Hexagon.Hexagon_Height*2-3*Hexagon.radius;
 
 
@@ -50,8 +50,11 @@ public Pane add() {
     			xCoord = xCoord + Hexagon.Hexagon_Width;
     		}
     		if(HexRowStart>5){
-    			if(j==(HexRowStart*2-2)/2){
-					xCoord = xCoord - Hexagon.Hexagon_Width;
+    			if(j>=5 && j<HexRowStart){
+					xCoord = xCoord - Hexagon.Hexagon_Width*((int)(j-3)/2);
+				}
+				if(j>=HexRowStart && j<HexRowStart*2-6){
+					xCoord = xCoord - Hexagon.Hexagon_Width*((int)(j-HexRowStart+2)/2);
 				}
 			}
 
