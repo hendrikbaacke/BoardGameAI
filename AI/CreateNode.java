2ed57ac042e2c5e03b0ee77560138641a58d16c3
Janneke13 <45425403+Janneke13@users.noreply.github.com>
2019-11-27 08:43:46 +0100

    Delete CreateNode.java

--- AI/CreateNode.java
+++
@@ -1,8 +0,0 @@
-package AI;
-
-public class CreateNode {
-		//can be used by addnodes maybe?? to check and store every move that's available
-		//if a node is not available, delete it
-	
-		//so basically, get a hashtable 
-}
