a25f7a5062d8324fbd5cafbca6ab56b785ac1c9e
Hendrik Simon Baacke <hendrikbaacke@gmail.com>
2019-12-09 15:41:13 +0100

    weights

--- AI/ModeDetermination_2.java
+++ AI/ModeDetermination_2.java
@@ -10,7 +10,7 @@ public class ModeDetermination_2 {
     //1. closing Distance, 2. Cohesion, 3.breakGroup, 4. strengthenGroup, 5. Difference Opponent Marbles, 6. Difference Own Marbles
 
 
-    private double[] mode1={  -1    ,  0  ,  0    ,  0  ,  500  ,  -1000 }; //get to the middle, except we can kill
+    private double[] mode1={  0    ,  0  ,  0    ,  0  ,  0  ,  0 }; //get to the middle, except we can kill
 
     private double[] mode2={  -0.2  ,  50 ,  200  ,  0  ,  0    ,  -1000 }; //1.breaking groups 2.cohesion
 
@@ -35,27 +35,28 @@ public class ModeDetermination_2 {
 
     public double[] determineMode_2(double centerDistance, double cohesion){
 
-        if(centerDistance> DistanceM1)
+    //    if(centerDistance> DistanceM1)
 
             return mode1;
-        if(centerDistance<DistanceM1)
-            return mode2;
-        if(centerDistance<=DistanceM1 && cohesion>=0 && cohesion<4)
-            return mode3;
-        if(centerDistance<=DistanceM1 && cohesion>=4 && cohesion<10)
-            return mode4;
-        if(centerDistance<=DistanceM1 && cohesion>=10 && cohesion<16)
-            return mode5;
-        if(centerDistance<=DistanceM1 && cohesion>=16 && cohesion<22)
-            return mode6;
-        if(centerDistance<=DistanceM1 && cohesion>=22 && cohesion<28)
-            return mode7;
-        if(centerDistance<=DistanceM1 && cohesion>=28 && cohesion<34)
-            return mode8;
-        if(centerDistance<=DistanceM1 && cohesion>=34)
-            return mode9;
 
-        return defaultMode;
+//        if(centerDistance<DistanceM1)
+//            return mode2;
+//        if(centerDistance<=DistanceM1 && cohesion>=0 && cohesion<4)
+//            return mode3;
+//        if(centerDistance<=DistanceM1 && cohesion>=4 && cohesion<10)
+//            return mode4;
+//        if(centerDistance<=DistanceM1 && cohesion>=10 && cohesion<16)
+//            return mode5;
+//        if(centerDistance<=DistanceM1 && cohesion>=16 && cohesion<22)
+//            return mode6;
+//        if(centerDistance<=DistanceM1 && cohesion>=22 && cohesion<28)
+//            return mode7;
+//        if(centerDistance<=DistanceM1 && cohesion>=28 && cohesion<34)
+//            return mode8;
+//        if(centerDistance<=DistanceM1 && cohesion>=34)
+//            return mode9;
+//
+//        return defaultMode;
 
 
     }
--- src/GameGui.java
+++ src/GameGui.java
@@ -40,17 +40,17 @@ public class GameGui extends Application {
 				if(Move.playersTurn==2){
 				AI.Strategies strategies = new AI.Strategies(new GameState(Board.hashBoard, Move.playersTurn));
 
-				double f1=strategies.closingDistance(new GameState(Board.hashBoard, Move.playersTurn));
+				//double f1 = strategies.closingDistance(new GameState(Board.hashBoard, Move.playersTurn));
 				//double f1=strategies.closingDistanceTest();
-				//double f2=strategies.cohesion();
+				double f2=strategies.cohesion();
 				//double f3=strategies.breakGroup();
 				//double f4=strategies.strengthenGroup();
 				//double f5=strategies.amountOppMarbles(state,isPlayer1AI);
 				//double f6=strategies.amountOwnMarbles(state,isPlayer1AI);
 
-				System.out.println(" -------- "+f1+" -------- ");}
+				System.out.println(" -------- "+f2+" -------- ");}
 				//System.out.println(Board.hashBoard.get("E5").centerX-Board.hashBoard.get("G4").centerX);
-				System.out.println( Math.sqrt(Math.pow(Board.hashBoard.get("E5").centerX- Board.hashBoard.get("G6").centerX, 2) + Math.pow(Board.hashBoard.get("E5").centerY - Board.hashBoard.get("G6").centerY, 2)));
+				//System.out.println( Math.sqrt(Math.pow(Board.hashBoard.get("E5").centerX- Board.hashBoard.get("G6").centerX, 2) + Math.pow(Board.hashBoard.get("E5").centerY - Board.hashBoard.get("G6").centerY, 2)));
 			});
 			
 			buttonAI.setOnAction(e -> {
